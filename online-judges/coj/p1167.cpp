df4f2e933672f23120a89e90bb6ab9ff140d4852
Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>
2017-03-16 08:38:03 -0600

    Add an improvement for Words problem
    Signed-off-by: Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>

--- online-judges/coj/p1167.cpp
+++ online-judges/coj/p1167.cpp
@@ -8,7 +8,8 @@ using namespace std;
 const int  MaxW = 17;           // Number of Words
 const char MaxZ = 5;            // Alphabet size
 int dp[1<<MaxW][MaxZ];          // DP Table
-int queue[3<<MaxW], head, tail; //Queue
+int queue[1<<MaxW], head, tail; //Queue
+bool seen[1<<MaxW];
 
 inline char getIdx(char c) {
   switch(c) {
@@ -16,7 +17,7 @@ inline char getIdx(char c) {
     case 'E': return 1;
     case 'I': return 2;
     case 'O': return 3;
-    case 'U': return 4;
+    default: return 4;
   }
 }
 
@@ -33,12 +34,13 @@ int main() {
       size [i] = strlen(word);
       begin[i] = getIdx(word[0]);
       end  [i] = getIdx(word[size[i]-1]);
-      //printf("w(%s), b(%d), e(%d), s(%d)\n",word,(int)begin[i],(int)end[i],size [i]);
     }
     // Clean up
-    for(int i=0; i<(1<<noWords); ++i)
+    for(int i=0; i<(1<<noWords); ++i) {
+      seen[i] = false;
       for(char j=0; j<MaxZ; ++j)
         dp[i][j] = 0;
+    }
     
     // Initialize queue and answer
     ans = 0; head=1; tail=0; queue[0]=0;
@@ -57,6 +59,8 @@ int main() {
             now = prv+size[wd];
             //printf("  new_mask(%d) size(%d)\n",new_mask,now);
             ans = max(ans, now);
+            if(seen[new_mask]) continue;
+            seen[new_mask] = true;
             queue[head] = new_mask;
             ++head;
           }
