2ad110b6d669b181cc406702746a0472c7cf2219
Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>
2017-06-14 23:55:25 -0500

    Improve solution for weekly problem
    Signed-off-by: Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>

--- weekly-training/week14/cf-22B/main.cpp
+++ weekly-training/week14/cf-22B/main.cpp
@@ -7,30 +7,26 @@ const int kMax = 30;
 int sa [kMax][kMax];
 
 int main() {
-  int R, C, v;
+  int N, M, v;
   string str;
 
-  cin >> R >> C;
+  cin >> N >> M;
   // Read and process
-  for(int r=1; r<=R; ++r) {
+  for(int r=1; r<=N; ++r) {
     cin >> str;
-    for(int c=1; c<=C; ++c) {
+    for(int c=1; c<=M; ++c) {
       v = str[c-1]-'0';
-      sa [r][c] = v + sa[r-1][c] +
-       sa[r][c-1] -  sa[r-1][c-1];
+      sa [r][c] = v + sa[r][c-1];
     }
   }
   // brute force
   int ans = 0;
-  for(int w=R; w>0; --w) {
-    for(int h=C; h>0; --h) {
-      for(int r=R; r>=w; --r) {
-        for(int c=C; c>=h; --c) {
-          int cnt = (sa[r][c] - sa[r-w][c]
-            - sa[r][c-h] + sa[r-w][c-h]);
-          if(cnt) continue;
-          ans = max(ans, (w<<1)+(h<<1));
-        }
+  for(int L=0; L<M; ++L) {
+    for(int R=M; R>L; --R) {
+      int H = 0;
+      for(int r=1; r<=N; ++r) {
+        H = (sa[r][R]-sa[r][L]) ? 0 : H+1;
+        if(H) ans = max(ans, (H*2)+((R-L)*2));
       }
     }
   }
