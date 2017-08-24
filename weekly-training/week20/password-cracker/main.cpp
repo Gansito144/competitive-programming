8785e3fe6953a3b412b13e99ee620e8abd33ef20
Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>
2017-08-23 23:06:35 -0500

    update solution for password checker

--- weekly-training/week20/password-cracker/main.cpp
+++ weekly-training/week20/password-cracker/main.cpp
@@ -1,12 +1,14 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <memory.h>
 #include <algorithm>
 
 using namespace std;
 
 const int       kMaxN = 10;
-vector <int>     ans(2003);
+vector <int> ans(2003);
+int dp[2003];
 vector <string> pwd(kMaxN); 
 
 // Needed for each test
@@ -14,18 +16,22 @@ int N;
 bool can;
 string tgt;
 
-void backtrack(int k, int len) {
+int backtrack(int k, int len) {
   // Check if solution
+  if(dp[len] != -1) return dp[len];
   if(len == tgt.size()) {
     for(int i=0; i<=k; ++i) {
       if(i) cout << " ";
       cout << pwd[ans[i]];
     }
-    cout << "\n"; can = true;
+    cout << "\n";
+    return dp[len] = 1;
   } else {
     ++k;
+    bool ret = false;
     // Look for candidates and go
     for(int i=0; i<N && !can; ++i) {
+      if(pwd[i].size() == 0) continue;
       if(len + pwd[i].size() > tgt.size()) continue;
       bool fit = true;
       for(int j=0; j<pwd[i].size(); ++j) {
@@ -36,9 +42,11 @@ void backtrack(int k, int len) {
       }
       if(fit) {
         ans[k] = i;
-        backtrack(k,len+pwd[i].size());
+        ret = backtrack(k,len+pwd[i].size());
       }
+      if(ret) return dp[len] = 1;
     }
+    return dp[len] = 0;
   }
 }
 
@@ -54,9 +62,17 @@ int main() {
       for(char & c: pwd[i]) msk_src |= 1<<(c-'a');
     }
     cin >> tgt;
-    for(char & c: tgt) msk_tgt |= 1<<(c-'a');
+    for(char & c : tgt) msk_tgt |= 1<<(c-'a');
+    for(int i=0; i<N; ++i) {
+        if(tgt.find(pwd[i]) == string::npos) {
+            pwd[i] = "";
+        }
+    }
     can = false;
-    if( (msk_tgt&msk_src) == msk_tgt) backtrack(-1, 0);
+    if( (msk_tgt&msk_src) == msk_tgt) {
+     memset(dp, -1, sizeof dp);
+     can = backtrack(-1, 0);
+    }
     if(!can) cout << "WRONG PASSWORD\n";
   }
   return 0;
