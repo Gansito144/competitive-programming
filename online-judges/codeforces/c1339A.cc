ddd79f4f419c564107f04a02781b132312841edd
Ulises Mendez <ulisesmdzmtz@gmail.com>
2020-05-24 23:26:37 -0700

    Add solution to test config

---
+++ online-judges/codeforces/c1339A.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  int t, n;
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    cout << n << "\n";
+  }
+  return 0;
+}
+
+
